f2ec436ba61ca7b3dbd4bcb602e413f38c7c9b6b
rashmi916 <rashmi.mr@cmr.edu.in>
2026-02-18 10:46:16 +0530

    updated all files

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 const openBtn = document.querySelector("#menu-open-button");
 const closeBtn = document.querySelector("#menu-close-button");
 const navMenu = document.querySelector(".nav-menu");
+const orderbutton = document.querySelector(".button")
+const contact = document.querySelector(".contact-us")
 
 openBtn.addEventListener("click", () => {
   navMenu.classList.add("active");
@@ -9,3 +11,4 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   navMenu.classList.remove("active");
 });
+
